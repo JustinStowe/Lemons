fb9733be9900cba6ef5bda83daf3a581331c3c6d
JustinStowe <110639329+JustinStowe@users.noreply.github.com>
2022-11-29 12:46:41 -0700

    decided I wanted functional quit game button, created coversheet with info and open window button

--- script.js
+++ script.js
@@ -6,20 +6,30 @@ CLASSES
 /* ======================
  GLOBAL VARS
 =========================*/
-
+let openedwindow;
 /* ======================
  DOM STUFF
 =========================*/
 const newGame = document.querySelector("#new-game");
 const loadGame = document.querySelector("#load-save");
+const closeGame = document.querySelector("#close");
+const openGame = document.querySelector("#open");
 /* =============================
 FUNCTIONS
 ============================= */
+function runGame() {
+  openedwindow = window.open("startscreen.html");
+}
 function startGame() {
   console.log("button works");
   window.location = "playerscreen.html";
 }
+function exitGame() {
+  openedwindow.close();
+}
 /* =============================
 EVENT LISTENERS
 ============================= */
-newGame.addEventListener("click", startGame);
+openGame.addEventListener("click", runGame);
+// newGame.addEventListener("click", startGame);
+closeGame.addEventListener("click", window.close());

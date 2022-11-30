59018dc59d98396ccffb67969d7babe62f424915
JustinStowe <110639329+JustinStowe@users.noreply.github.com>
2022-11-29 19:11:15 -0700

    back to the drawing board! created dedicated project trinities for each page of my project(html,css,JS)

--- script.js
+++ script.js
@@ -10,24 +10,11 @@ CLASSES
 /* ======================
  DOM STUFF
 =========================*/
-const newGame = document.querySelector("#new-game");
-const loadGame = document.querySelector("#load-save");
-const closeGame = document.querySelector("#close");
-const openGame = document.querySelector("#open");
+
 /* =============================
 FUNCTIONS
 ============================= */
-function runGame() {
-  window.location = "startscreen.html";
-}
-function startGame() {
-  console.log("button works");
-  window.location = "playerscreen.html";
-}
-function exitGame() {
-  window.location = "index.html";
-}
+
 /* =============================
 EVENT LISTENERS
 ============================= */
-newGame.addEventListener("click", startGame);

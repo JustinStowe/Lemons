0ca9b699b9e2cbc62df8df5f3a7e393e111866f1
JustinStowe <110639329+JustinStowe@users.noreply.github.com>
2022-12-03 14:56:41 -0700

    completed full functionality of upgrades modal. If player has enough money to buy the tool, tool cost is subtracted, tool is added to player.tool array and buy button is replaced with OWNED.

--- script3.js
+++ script3.js
@@ -58,9 +58,10 @@ class Tool {
   }
 }
 class Locale {
-  constructor(name, rent, people, popularity) {
+  constructor(name, rent, info, people, popularity) {
     this.name = name;
     this.rent = rent;
+    this.info = info;
     this.people = people;
     this.popularity = popularity;
   }
@@ -78,11 +79,41 @@ const jennifer = new Hireling("Jennifer", 100, 50, 5);
 const RonPopeil = new Hireling("Ron Popeil", 200, 100, 20);
 const daleCarnegie = new Hireling("Dale Carnegie", 1000, 500, 100);
 /* Class seperation */
-const neighborhood = new Locale("neighborhood", 0, 10, 1);
-const park = new Locale("park", 5, 20, 0);
-const downTown = new Locale("down town", 15, 40, 0);
-const beach = new Locale("beach", 30, 120, 0);
-const hexaStadium = new Locale("The Hexa-Stadium", 100, 800, 0);
+const neighborhood = new Locale(
+  "neighborhood",
+  0,
+  "In front of your parents house is the perfect place to start selling lemonade, and it will not cost a penny in rent.",
+  10,
+  1
+);
+const park = new Locale(
+  "park",
+  5,
+  "A lemonade stand at the park? Great idea, many people play and exercise at the park and would enjoy relieving their thirst with lemonade. ",
+  20,
+  0
+);
+const downTown = new Locale(
+  "down town",
+  15,
+  "There is always people and tourist down town bustling around and would appreciate refreshing lemonade ",
+  40,
+  0
+);
+const beach = new Locale(
+  "beach",
+  30,
+  "What could be better than having fun at the beach  and being able to enjoy refreshing lemonade.",
+  120,
+  0
+);
+const hexaStadium = new Locale(
+  "The Hexa-Stadium",
+  100,
+  "You better bring your A-game to the game, the masses of fans will want something to wash down those hotdogs",
+  800,
+  0
+);
 /* ======================
  GLOBAL VARS
 =========================*/
@@ -116,6 +147,7 @@ const localeInfo = document.querySelector(".locales");
 const nextLocale = document.querySelector(".next-locale");
 const prevLocale = document.querySelector(".prev-locale");
 const chooseLocale = document.querySelector(".this-locale");
+const upgradeItemList = document.querySelector(".upgrade-items");
 /* ==========modals===============*/
 const statsModal = document.querySelector(".stats-modal");
 const rentModal = document.querySelector(".rent-modal");
@@ -165,6 +197,7 @@ function populateLocales() {
   <h2>choose where you want to set up your stand and how much it will cost you.</h2>
                 <img class="locale-pic" src="${localePics[currentLocaleIndex]}" alt="test" height="374px" width="463px">
           <h3>${locales[currentLocaleIndex].name}</h3>
+          <p>${locales[currentLocaleIndex].info}</p>
           <h4>costs: ${locales[currentLocaleIndex].rent} daily
   `;
 }
@@ -188,24 +221,124 @@ function localeBtns(evt) {
   }
   populateLocales();
 }
-function setupTools() {
-  upgradeModal.innerHTML = `
-  <h2>Lets get some upgrades</h2>
-            <ul class="upgrade-items">
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-                <li><img src="" alt=""> <button>Buy</button></li>
-            </ul>
-
+function populateTools() {
+  upgradeItemList.innerHTML = `
+    <li id="umbrella"><button class="umbrella button">Buy</button><img src="" alt=""> Umbrella Cost: 99</li>
+      <p>customers are more likely to stop to buy lemonade</p>
+    <li id="ice-machine"><button class = "ice-machine button">Buy</button><img src="" alt=""> Ice machine Cost: 99</li>
+      <p>automatically give you 100 ice to start each day</p>
+    <li id="boom-box"><button class="boombox button">Buy</button><img src="" alt=""> BoomBox Cost: 149</li>
+      <p>Attracts more potential customers to you lemonade stand</p>
+    <li id="juicer"><button class="juicer button">Buy</button><img src="" alt=""> Juicer Cost: 199</li>
+      <p>reduces the time it takes to make another pitcher of lemonade</p>
+    <li id="cash-register"><button class="cash-register button">Buy</button><img src="" alt=""> cash Register Cost: 350</li>
+      <p>reduces the time it takes to complete a transaction</p>
+    <li id="fridge"><button class="fridge button">Buy</button><img src="" alt=""> Fridge Cost: 800</li>
+      <p>keeps ice from melting and keeps your lemons from spoiling. </p>
   `;
+
+  player.tools.forEach((index) => {
+    if (player.tools.includes(umbrella)) {
+      const umbrellaLi = document.querySelector("#umbrella");
+      umbrellaLi.innerHTML = `<span>OWNED</span><img src="" alt=""> Umbrella Cost: 99`;
+    }
+    if (player.tools.includes(iceMachine)) {
+      const iceMachineLi = document.querySelector("#ice-machine");
+      iceMachineLi.innerHTML = `<span>OWNED</span><img src="" alt=""> Ice machine Cost: 99`;
+    }
+    if (player.tools.includes(boomBox)) {
+      const boomBoxLi = document.querySelector("#boom-box");
+      boomBoxLi.innerHTML = `<span>OWNED</span><img src="" alt=""> BoomBox Cost: 149`;
+    }
+    if (player.tools.includes(juicer)) {
+      const juicerLi = document.querySelector("#juicer");
+      juicerLi.innerHTML = `<span>OWNED</span><img src="" alt=""> Juicer Cost: 199`;
+    }
+    if (player.tools.includes(cashRegister)) {
+      const cashRegisterLi = document.querySelector("#cash-register");
+      cashRegisterLi.innerHTML = `<span>OWNED</span><img src="" alt=""> cash Register Cost: 350`;
+    }
+    if (player.tools.includes(fridge)) {
+      const fridgeLi = document.querySelector("#fridge");
+      fridgeLi.innerHTML = `<span>OWNED</span><img src="" alt=""> Fridge Cost: 800`;
+    }
+  });
+  const toolBuyBtns = document
+    .querySelector(".upgrade-items")
+    .querySelectorAll(".button");
+  toolBuyBtns.forEach((item) => {
+    item.addEventListener("click", buyTools);
+  });
+}
+function buyTools(evt) {
+  if (evt.target.classList.contains("umbrella")) {
+    if (player.money >= umbrella.cost) {
+      player.money -= umbrella.cost;
+      player.tools.push(umbrella);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
+  if (evt.target.classList.contains("ice-machine")) {
+    if (player.money >= iceMachine.cost) {
+      player.money -= iceMachine.cost;
+      player.tools.push(iceMachine);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
+  if (evt.target.classList.contains("boombox")) {
+    if (player.money >= boomBox.cost) {
+      player.money -= boomBox.cost;
+      player.tools.push(boomBox);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
+  if (evt.target.classList.contains("juicer")) {
+    if (player.money >= juicer.cost) {
+      player.money -= juicer.cost;
+      player.tools.push(juicer);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
+  if (evt.target.classList.contains("cash-register")) {
+    if (player.money >= cashRegister.cost) {
+      player.money -= cashRegister.cost;
+      player.tools.push(cashRegister);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
+  if (evt.target.classList.contains("fridge")) {
+    if (player.money >= fridge.cost) {
+      player.money -= fridge.cost;
+      player.tools.push(fridge);
+      populateTools();
+    } else {
+      alert("you don't have enough money for this");
+    }
+  }
 }
 function saveMyGame() {}
+/* =============================
+PLAYER
+============================= */
+const player = {
+  money: 20,
+  locale: locales[currentLocaleIndex],
+  lemons: 0,
+  sugar: 0,
+  cups: 0,
+  ice: 0,
+  tools: [],
+};
 /* =============================
 EVENT LISTENERS
 ============================= */
@@ -228,16 +361,5 @@ startDay.addEventListener("click", () => {
 });
 window.onload = () => {
   populateLocales();
-};
-/* =============================
-PLAYER
-============================= */
-const player = {
-  money: 20,
-  locale: locales[currentLocaleIndex],
-  lemons: 0,
-  sugar: 0,
-  cups: 0,
-  ice: 0,
-  tools: [],
+  populateTools();
 };
